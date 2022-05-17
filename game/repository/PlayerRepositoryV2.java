d335532bf43e84b6ddc49e6ef519804d033c86ce
DinaEll <90531300+DinaEll@users.noreply.github.com>
2022-05-18 00:37:58 +0300

    Delete game directory

--- game/repository/PlayerRepositoryV2.java
+++
@@ -1,9 +0,0 @@
-package com.game.repository;
-
-import com.game.entity.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepositoryV2 extends JpaRepository<Player, Long> {}
-
